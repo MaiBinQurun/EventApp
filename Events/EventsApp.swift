7cade051441e4b7d2873a0b65f3195fa7876cc22
pko <kjlk>
2023-01-09 14:29:57 +0300

    Initial Commit

---
+++ Events/EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsApp.swift
+//  Events
+//
+//  Created by mai abdullah qurun on 16/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
